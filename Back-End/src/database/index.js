9b2083d3e6a3d4853d86d0793cf79063b20fb05b
Abner Sasntos <abnersenna@hotmail.com>
2022-11-18 16:36:40 -0300

    projeto organizado por pastas

--- Back-End/src/database/index.js
+++ Back-End/src/database/index.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
+require('dotenv').config()
+
+const DB_USER = process.env.USER
+const DB_PASSWORD = process.env.PASSWORD
+
 mongoose.connect(
-  `mongodb+srv://API-Campinho:campinho10@apicampinho.1yipwgr.mongodb.net/?retryWrites=true&w=majority`,
+  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicampinho.1yipwgr.mongodb.net/?retryWrites=true&w=majority`,
   {},
   error => {
     if (error) {
--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const authcontroller = require('./controller/AuthController')
+
 const app = express()
+
+require('dotenv').config()
 
 app.use(express.json())
 
